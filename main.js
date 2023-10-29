49122f44901e175a7550d35b55978683f9b5400a
jose-tl <j.tonnesenlucas@gmail.com>
2023-10-29 20:59:24 +0100

    Finalizing css, finalizing content, adding media responsiveness

--- main.js
+++ main.js
@@ -5,18 +5,44 @@
 const food = [
     {
         name: "coffee",
-        lethal: 150, /* mL/kg */
+        lethal: 192, /* mL/kg */
+        measure: "L",
+        ratio: 0.004,/* how many mL to a cup?*/
+        container: "mugs of coffee"
+    },
+    {
+        name: "cola",
+        lethal: 192, /* mL/kg */
+        measure: "L",
+        ratio: 0.5,/* how many mL to a bottle?*/
+        container: "bottles of cola"
     },
     {
         name: "water",
-        lethal: 300, /* mL/kg */
+        lethal: 90000,
+        measure: "L",/* mL/kg */
+        ratio: 0.004,
+        container: "cups of water"
     },
     {
-        name: "apples",
+        name: "apple",
         lethal: 50, /* mg/kg */
+        measure: "g",
+        ratio: 1/100000,
+        container: "apples"
+    },
+    {
+        name: "banana",
+        lethal: 33333333, /* mg/kg */
+        measure: "g",
+        ratio: 0.00001,
+        container: "bananas"
     }
+
 ]
 const calcBtn = document.querySelector("#calc-btn")
+const middleWrapper = document.querySelector(".middle-wrapper")
+
 
 let lethalDoseResult = 0;
 
@@ -24,39 +50,54 @@ let lethalDoseResult = 0;
           Functions
     ======================= */
 function getSelectedFood() {
+    let selectedFood = {}
     const foodElem = document.querySelector("#food-select")
-    return foodElem.options[foodElem.selectedIndex].text
-}
+    const foodText = foodElem.options[foodElem.selectedIndex].value
 
-function getLethalDose(foodText) {
-    let lethalReturn = 0;
     food.forEach((foodObj) => {
-        if(foodObj.name === foodText.toLowerCase()) lethalReturn = foodObj.lethal
+        if(foodObj.name === foodText.toLowerCase()) selectedFood = foodObj
     })
-    return lethalReturn
+
+    return selectedFood
 }
 
 function getWeight(){
     const weightElem = document.querySelector("#weight")
-    if (typeof weightElem.value === "string") return weightElem.placeholder;
+    if (weightElem.value === "") return weightElem.placeholder;
     return weightElem.value
 }
 
-function calculate () {
-    const weight = getWeight();
-    const food = getSelectedFood();
-    const lethal = getLethalDose(food);
-
-    lethalDoseResult = lethal*weight;
-}
-
 function calcCssTransform(){
+
     calcBtn.innerHTML = "<span id=\"btn-loader\"></span>"
+    calcBtn.disabled = true
+
     setTimeout(()=>{
-        calcBtn.innerHTML = "Result"
+        calcBtn.innerHTML = "Try again!"
+        calcBtn.disabled = false
+        middleWrapper.classList.replace("middle-wrapper","middle-wrapper-active")
+        insertCalcResults();
     }, 2000);
 }
 
+function insertCalcResults(){
+    const choiceSpan = document.querySelectorAll(".food-choice")
+    const lethalSpan = document.querySelectorAll("#food-lethal")
+    const containerSpan = document.querySelectorAll("#food-container")
+
+    const weight = getWeight();
+    const food = getSelectedFood();
+    const foodDose = weight*food.lethal
 
 
-calcBtn.onclick = ()=>{calculate(); calcCssTransform()};
+    choiceSpan.forEach((elem)=>elem.innerText = food.name)
+    lethalSpan.forEach((elem)=> elem.innerText = Math.round(((foodDose/1000)*100)/100) + " " + food.measure)
+    containerSpan.forEach((elem)=>elem.innerText = Math.ceil(food.ratio*foodDose) + " " + food.container)
+
+}
+
+
+
+
+
+calcBtn.onclick = ()=>{ calcCssTransform()};
